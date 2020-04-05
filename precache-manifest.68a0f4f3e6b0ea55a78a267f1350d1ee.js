bb55141939686b3039f3d87ef9476000b8630e77
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-04-05 03:02:33 -0400

    Updates

---
+++ precache-manifest.68a0f4f3e6b0ea55a78a267f1350d1ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a46e2fa2fca870a4276dca0498156a",
+    "url": "/project-portfolio/index.html"
+  },
+  {
+    "revision": "a81e7e304aba26b47776",
+    "url": "/project-portfolio/static/css/main.0ed71535.chunk.css"
+  },
+  {
+    "revision": "fa434f82924a7a155415",
+    "url": "/project-portfolio/static/js/2.8d56277d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/project-portfolio/static/js/2.8d56277d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81e7e304aba26b47776",
+    "url": "/project-portfolio/static/js/main.e3128d39.chunk.js"
+  },
+  {
+    "revision": "3e0df6e4da4157ae53f5",
+    "url": "/project-portfolio/static/js/runtime-main.2ea9d4d9.js"
+  },
+  {
+    "revision": "d3ac744b10e647153d04f7f0c7151313",
+    "url": "/project-portfolio/static/media/budget.d3ac744b.jpg"
+  },
+  {
+    "revision": "e4522b9df13f30176496ca1323826bac",
+    "url": "/project-portfolio/static/media/face.e4522b9d.jpg"
+  },
+  {
+    "revision": "ce0abb047cf06a67af7ed586742ddc76",
+    "url": "/project-portfolio/static/media/hotel.ce0abb04.jpeg"
+  },
+  {
+    "revision": "f4f0fd7bd29070d7d238b841cbfcf2d3",
+    "url": "/project-portfolio/static/media/logo.f4f0fd7b.png"
+  }
+]);
